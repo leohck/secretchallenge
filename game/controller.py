be8bb5d3e6eda5928dcd67e738bdf1fc511f1463
leohck <leonardoblack.net@gmail.com>
2021-05-02 12:29:01 -0300

    Criada classe Controller , responsável por executar n partidas e analisa-las
    Criado arquivo main.py para fazer o uso da aplicação

---
+++ game/controller.py
@@ -0,0 +1,67 @@
+from collections import Counter
+from game.match import Match
+
+
+class Controller:
+    """
+    Controlador do Jogo
+    """
+
+    def __init__(self):
+        self.data = {
+            "matches": 0,
+            "rounds": [],
+            "winners": []
+        }
+
+    def analyze_data(self):
+        """
+        Faz a analise dos dados das partidas e retorna um dicionario contendo a analise.
+        para utilizar este metodo é necessario que execute as partidas antes (self.run_matches)
+        :return: Dict
+        """
+        assert self.data["rounds"], "You need to run the matches before analyzing the data"
+
+        matches = self.data['matches']
+        rounds_counter = Counter(self.data['rounds'])
+        winners_counter = Counter(self.data['winners'])
+
+        timeout_matches = rounds_counter[1000]
+        average_rounds_match = round(sum(self.data["rounds"]) / len(self.data["rounds"]))
+        most_winning_behavior = max(winners_counter, key=winners_counter.get)
+
+        winners = dict(sorted(winners_counter.items(), key=lambda kv: kv[1], reverse=True))
+        behaviors = []
+        for behavior, won_matches in winners.items():
+            win_rate = (won_matches * 100) / matches
+            behaviors.append(
+                {
+                    'behavior': behavior,
+                    'won_matches': won_matches,
+                    'win_rate': win_rate
+                }
+            )
+
+        analyzed_data = {
+            'matches': matches,
+            'timeout_matches': timeout_matches,
+            'average_rounds_match': average_rounds_match,
+            'most_winning_behavior': most_winning_behavior,
+            'behaviors': behaviors
+        }
+        return analyzed_data
+
+    def run_matches(self, matches=300, analyze=False):
+        """
+        Executa n partidas e se necessario retorna a analise desse conjunto de partidas.
+        :param matches: Numero de partidas
+        :param analyze: Flag para saber se é necessario retornar a analise ou não
+        :return: Dict or None
+        """
+        self.data["matches"] += matches
+        for i in range(matches):
+            analysis = Match(i).run()
+            self.data['rounds'].append(analysis['rounds'])
+            self.data['winners'].append(analysis['winner'])
+        if analyze:
+            return self.analyze_data()
---
+++ main.py
@@ -0,0 +1,50 @@
+"""
+Created by: Leonardo Black
+02/05/2021
+
+Arquivo responsavel por iniciar as partidas e analisa-las
+
+Saida:
+    - Quantas partidas terminam por time out (1000 rodadas);
+    - Quantos turnos em média demora uma partida;
+    - Qual a porcentagem de vitórias por comportamento dos jogadores;
+    - Qual o comportamento que mais vence
+"""
+
+from game.controller import Controller
+
+
+if __name__ == '__main__':
+    """
+    Executa as 300 partidas e imprime no console os dados referentes às execuções.
+    Observação é possível encadear execuções de partidas e analisar tudo no final,
+    Exemplo:
+    gc = Controller()
+    gc.run_matches(100)
+    gc.run_matches(100)
+    gc.run_matches(100)
+    analysis = gc.analyze_date()
+    """
+    gc = Controller()
+    gc.run_matches(300)
+    analysis = gc.analyze_data()
+    print(f'Numero de partidas rodadas: {analysis["matches"]}')
+
+    print(
+        'Quantas partidas terminam por time out(1000 rodadas)? \n'
+        f'{analysis["timeout_matches"]}'
+    )
+
+    print(
+        'Quantos turnos em média demora uma partida? \n'
+        f'{analysis["average_rounds_match"]}'
+    )
+
+    print('Qual a porcentagem de vitórias por comportamento dos jogadores? ')
+    for behavior in analysis["behaviors"]:
+        print(f'{behavior["behavior"]}: {behavior["win_rate"]:.2f}%')
+
+    print(
+        'Qual é o comportamento que mais vence? \n'
+        f'{analysis["most_winning_behavior"]}'
+    )
